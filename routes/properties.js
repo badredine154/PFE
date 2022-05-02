66ad6666122c8561eb9ae8079e4838d362433523
badredine154 <87574290+badredine154@users.noreply.github.com>
2022-05-02 03:48:58 +0100

    push

--- routes/properties.js
+++ routes/properties.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const properties = [
+let properties = [
 ]
 
 router.get('/', (req, res) => {
@@ -16,6 +16,24 @@ router.post('/', (req, res) => {
     properties.push({...property, id: uuidv4()});
 
     res.send(`property with name : ${property.title} added to the database`);
-})
+});
+
+router.get('/:id',(req,res) => {
+    const { id } = req.params;
+
+    const foundProperty = properties.find((property) => property.id === id);
+
+    res.send(foundProperty);
+});
+
+router.delete('/:id', (req,res) => {
+    const { id } = req.params;
+
+    properties = properties.filter((property) => property.id !== id);
+
+    res.send(`Property with the id${id} deleted`);
+
+
+});
 
 export default router;
