57d4ed7ede6e238bf048f2f43e80ee10299dafd0
badredine154 <87574290+badredine154@users.noreply.github.com>
2022-05-12 02:41:27 +0100

    push

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ import cors from 'cors';
 import propertiesRoutes from './routes/properties.js';
 import mongoose from 'mongoose';
 
+import AdminBro from 'admin-bro';
+import expressAdminBro from '@admin-bro/express';
+import mongooseAdminBro from '@admin-bro/mongoose';
+
+import property from './models/property.js';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+AdminBro.registerAdapter(mongooseAdminBro);
+const AdminBroOptions = { resources: [property] }
+
+const adminBro = new AdminBro(AdminBroOptions);
+const router = expressAdminBro.buildRouter(adminBro);
+
+app.use(adminBro.options.rootPath, router);
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,5 +32,5 @@ app.get('/', (req, res) => {
 })
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.bz5zg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
-    .then(() => app.listen(PORT, () => console.log(`server running on port : http://localhost:${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`server running on port : http://localhost:${PORT}/admin`)))
     .catch((error) => console.log(error.message));
